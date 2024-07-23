de5501a4975cf6f268e8ff6573ce5092c242598e
Ahtisham8952 <ahtisham.badar786@gmail.com>
2024-07-23 17:19:50 +0500

    base APi url

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -15,7 +15,7 @@ export const SigninForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("/api/users/login", {
+      const response = await axios.post(`${API_BASE_URL}/api/users/login`, {
         email,
         password,
       });
