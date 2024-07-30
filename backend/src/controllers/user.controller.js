a38ca2fec63da85e916c02ceab244251af23fc32
Ahtisham8952 <ahtisham.badar786@gmail.com>
2024-07-30 19:57:57 +0500

    avatar not uploaded

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -47,12 +47,12 @@ const registerUser = asyncHandler(async (req, res) => {
     // }
 
     // Upload files to Cloudinary
-    const avatar = await uploadOnCloudinary(avatarLocalPath);
-    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+    // const avatar = await uploadOnCloudinary(avatarLocalPath);
+    // const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
-    if (!avatar) {
-        throw new ApiError(400, "Failed to upload avatar file");
-    }
+    // if (!avatar) {
+    //     throw new ApiError(400, "Failed to upload avatar file");
+    // }
 
     // Create user
     const user = await User.create({
