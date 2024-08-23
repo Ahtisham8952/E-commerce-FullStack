e477e57aa77bb10efbdb4ec2e01c3d8cba3bd94d
Ahtisham8952 <ahtisham.badar786@gmail.com>
2024-08-23 23:05:24 +0500

    public/temp file issue

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,13 +1,21 @@
-import multer from "multer"
+import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/temp')
-    },
-    filename: function (req, file, cb) {
-     
-      cb(null, file.originalname)
+  destination: function (req, file, cb) {
+    const destPath = process.env.NODE_ENV === 'production' ? '/tmp' : './public/temp';
+    
+    // Check if the directory exists, if not, create it
+    if (!fs.existsSync(destPath)) {
+      fs.mkdirSync(destPath, { recursive: true });
     }
-  })
-  
- export  const upload = multer({ storage: storage })
+    
+    cb(null, destPath);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({ storage: storage });
